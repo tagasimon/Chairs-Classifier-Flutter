57fc013fb35a0cc2a172fdb24f01a513e0d0e553
Simon Sayz <smkabz@gmail.com>
2021-05-11 17:49:27 +0300

    initial commit : no model trained

---
+++ android/app/src/main/kotlin/com/example/nhop_chairs_classifier/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nhop_chairs_classifier
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,167 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _loading = true;
+  File _image;
+  List _output;
+
+  final picker = ImagePicker();
+
+  pickImageCamera() async {
+    var image = await picker.getImage(source: ImageSource.camera);
+    if (image == null) return null;
+    setState(() {
+      _image = File(image.path);
+    });
+    classifyImage(_image);
+  }
+
+  pickImageGallery() async {
+    var image = await picker.getImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _image = File(image.path);
+    });
+    classifyImage(_image);
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 2,
+        threshold: 0.5,
+        imageMean: 127.5,
+        imageStd: 127.5);
+    setState(() {
+      _output = output;
+      _loading = false;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model_unquant.tflite", labels: "assets/labels.txt");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((val) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Tflite.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title"),
+        actions: [
+          IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text("Chairs Classifier CNN"),
+            Center(
+              child: _loading
+                  ? Container(
+                      width: 200,
+                      child: Column(
+                        children: [
+                          Image.asset("assets/cat.png"),
+                          SizedBox(
+                            height: 50.0,
+                          )
+                        ],
+                      ),
+                    )
+                  : Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 250,
+                            child: Image.file(_image),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          _output != null
+                              ? Text(
+                                  "${_output[0]}",
+                                  style: TextStyle(
+                                      color: Colors.purple, fontSize: 20),
+                                )
+                              : Container()
+                        ],
+                      ),
+                    ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      pickImageCamera();
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 100,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(6)),
+                      child: Text(
+                        "Take a Photo",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      pickImageGallery();
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 100,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(6)),
+                      child: Text(
+                        "Camera Roll",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
